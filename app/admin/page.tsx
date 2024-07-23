86ec3c869a529942252b484c37178475378623ad
utibe edet <utibeabasi.okon@myrecharge.ng>
2024-07-23 12:10:43 -0700

    care pulse

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -55,7 +55,7 @@ const AdminPage = async () => {
           />
         </section>
 
-         <DataTable columns={columns} data={appointments.documents} /> 
+         {/* <DataTable columns={columns} data={appointments.documents} />  */}
       </main>
     </div>
   );
